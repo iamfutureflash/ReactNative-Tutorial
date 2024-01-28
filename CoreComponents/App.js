8d7390c0ef7b3f5e9ce8f1a89b3a8b2b2b5b73e8
Puneet Vaishnav <puneet@coreteams.us>
2024-01-28 17:38:56 +0530

    understand how Text component is working

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,9 +1,15 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 export default function App() {
-  return (<>
+  return (
     <View style={{ backgroundColor: 'plum', flex: 1, }}>
-      <View style={{ backgroundColor: 'lightblue', width: 200, height: 200, borderRadius:8}}></View>
-      <View style={{ backgroundColor: 'lightgreen', width: 200, height: 200, borderRadius:8}}></View>
+      {/* <View style={{ backgroundColor: 'lightblue', width: 200, height: 200, borderRadius: 8 }}></View> */}
+      {/* <View style={{ backgroundColor: 'lightgreen', width: 200, height: 200, borderRadius: 8 }}></View> */}
+      <View style={{ backgroundColor: 'lightgreen', width: 200, height: 200, borderRadius: 8, padding: 60 }}>
+        <Text>
+          <Text style={{ color: 'white' }}>Hello</Text>
+          World
+        </Text>
+      </View>
     </View>
-  </>)
+  )
 }
