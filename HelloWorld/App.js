2647e96a54e5d79f0cfc348f080609c553d04e71
iamfutureflash <xenode2000@gmail.com>
2024-03-27 16:58:30 +0530

    take revision about scroll view

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, } from 'react-native';
-
-export default function App() {
+import { Text, View, Image, ImageBackground, ScrollView, Button, } from "react-native"
+const logoImage = require("./assets/adaptive-icon.png")
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 30, }}>
+      <ScrollView>
+        <Text><Text style={{ color: "white" }}>Hello</Text> World</Text>
+        <Image source={logoImage} style={{ width: 300, height: 300, }} />
+        <Image source={{ uri: "https://picsum.photos/200" }} style={{ width: 300, height: 300, }} />
+        <ImageBackground source={{ uri: "https://picsum.photos/200" }} style={{ width: 300, height: 300, }} >
+          <Text style={{ color: "white" }}>hi</Text>
+        </ImageBackground>
+        <ImageBackground source={logoImage} style={{ width: 300, height: 300, }} >
+          <Text style={{ color: "white" }}>hi</Text>
+        </ImageBackground>
+        <Button
+          title="Press"
+          onPress={() => { console.log("Button Pressed") }}
+          color="midnightblue"
+          disabled={true}
+        />
+      </ScrollView>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff232',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
