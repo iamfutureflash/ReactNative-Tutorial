7edd33185871f3348f25b701e4be310d08cd87dd
iamfutureflash <xenode2000@gmail.com>
2024-03-28 13:58:52 +0530

    learned about <Pressable           onPress={() => { console.log("Text on pressed") }}           onPressIn={() => { console.log("Text on PressIn") }}           onLongPress={() => { console.log("Text on long press pressed") }}           onPressOut={() => { console.log("Text on press out pressed") }}         > component

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,11 +1,43 @@
-import { Text, View, Image, ImageBackground, ScrollView, Button, } from "react-native"
+import { Text, View, Image, ImageBackground, ScrollView, Button, Pressable, } from "react-native"
 const logoImage = require("./assets/adaptive-icon.png")
 const App = () => {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 30, }}>
       <ScrollView>
-        <Text><Text style={{ color: "white" }}>Hello</Text> World</Text>
-        <Image source={logoImage} style={{ width: 300, height: 300, }} />
+        <Pressable
+          onPress={() => { console.log("Text on pressed") }}
+          onPressIn={() => { console.log("Text on PressIn") }}
+          onLongPress={() => { console.log("Text on long press pressed") }}
+          onPressOut={() => { console.log("Text on press out pressed") }}
+        >
+          <Text>
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+            Hello World
+          </Text>
+        </Pressable>
+
+        <Pressable
+          onPressIn={() => { console.log("Image onPressIn") }}
+          onPress={() => { console.log("Image on pressed") }}
+          onLongPress={() => { console.log("Image on long press pressed") }}
+          onPressOut={() => { console.log("Image on press out pressed") }}
+        >
+          <Image source={logoImage} style={{ width: 300, height: 300, }} />
+        </Pressable>
+
         <Image source={{ uri: "https://picsum.photos/200" }} style={{ width: 300, height: 300, }} />
         <ImageBackground source={{ uri: "https://picsum.photos/200" }} style={{ width: 300, height: 300, }} >
           <Text style={{ color: "white" }}>hi</Text>
